d39e1d458537377552d3e3dc376ceda63bdfa298
nudoru <kheavy@nudoru.com>
2015-12-03 17:22:14 -0500

    load json

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import Greeting from './components/greeting';
 import Header from './components/header';
 import AppStore from './stores/appStore';
-import LoadConfig from './loadConfig.js';
-//import Actions from './actions/actionCreators.js';
-//import Rest from '../nori/service/Rest.js';
+import LoadConfig from './service/JSONLoader.js';
+import Actions from './actions/actionCreators.js';
 
 require('!style!css!sass!../../sass/pages/_application.sass');
 require('!style!css!sass!../../sass/layout/_header.sass');
@@ -18,7 +17,14 @@ AppStore.subscribe(() => {
   console.log('Store updated', AppStore.getState());
 });
 
-LoadConfig.load();
+// Load local config JSON
+LoadConfig.onSuccess((data) => {
+  console.log('success!', data);
+  AppStore.dispatch(Actions.configLoaded());
+  AppStore.dispatch(Actions.setConfig(data));
+});
+LoadConfig.onError((err) => console.log('error!', err));
+LoadConfig.load('config.json');
 
 ReactDOM.render(
   <div id="app__container">
---
+++ src/scripts/app/service/JSONLoader.js
@@ -0,0 +1,36 @@
+import Rest from '../../nori/service/Rest.js';
+import Obs from '../../nori/extras/ObservableSubject.js'
+
+let _observable = Obs();
+
+export default {
+
+  promise: null,
+
+  onError(handler) {
+    _observable.subscribe('error', handler);
+  },
+
+  onSuccess(handler) {
+    _observable.subscribe('success', handler);
+  },
+
+  load(filename) {
+    if(!filename) {
+      console.warn('JSONLoader: Must specify file name');
+      return;
+    }
+
+    this.promise = Rest.request({
+      method: 'GET',
+      url   : filename,
+      json  : true
+    }).then((data) => {
+        _observable.notify('success', data);
+      }, (err) => {
+      _observable.notify('error', err);
+      }
+    );
+  }
+
+}
