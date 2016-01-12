95b491803675ad36e354810c4999c8856b86fbd4
nudoru <kheavy@nudoru.com>
2016-01-12 12:40:22 -0500

    rename

---
+++ src/scripts/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppStore from './stores/AppStore';
+import Actions from './actions/actionCreators.js';
+import ApplicationContainer from './containers/AppContainer.js';
+
+// Polyfill for IE11
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+if (typeof Object.assign != 'function') {
+  (function () {
+    Object.assign = function (target) {
+      'use strict';
+      if (target === undefined || target === null) {
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
+
+      var output = Object(target);
+      for (var index = 1; index < arguments.length; index++) {
+        var source = arguments[index];
+        if (source !== undefined && source !== null) {
+          for (var nextKey in source) {
+            if (source.hasOwnProperty(nextKey)) {
+              output[nextKey] = source[nextKey];
+            }
+          }
+        }
+      }
+      return output;
+    };
+  })();
+}
+
+// Polyfill Promise for IE11
+require('es6-promise').polyfill();
+
+// Start the app
+ReactDOM.render(<ApplicationContainer config="config.json"/>, document.querySelector('#application'));
