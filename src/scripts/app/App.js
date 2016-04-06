7f48ab3cb6e0534850620f3c5318a5e3721d674a
Matt Perkins <mperkins@redhat.com>
2016-04-06 16:55:04 -0400

    elevation and colors on spinner

--- src/scripts/app/App.js
+++ src/scripts/app/App.js
@@ -9,7 +9,6 @@ import React from 'react';
 import AppStore from './stores/AppStore';
 import Actions from './actions/actionCreators.js';
 import CoverMesage from './components/CoverMessage.js';
-import AppLoadingErrorView from './components/AppLoadingError.js';
 import Routes from './config/routes';
 import JSONLoader from '../nori/service/JSONLoader.js';
 
@@ -33,7 +32,7 @@ export default class AppContainer extends React.Component {
       AppStore.dispatch(Actions.setConfig(null));
       this.setState({loading: false});
     } else {
-      // Start loading the data
+      // Load config data
       JSONLoader.onSuccess((data) => {
         AppStore.dispatch(Actions.setConfig(data));
         this.setState({loading: false})
@@ -45,11 +44,14 @@ export default class AppContainer extends React.Component {
 
   // Render the application view depending on loading/error or data loaded
   render() {
+    return <CoverMesage heading='Please Wait' spinner='true'/>;
+    
+
     // If we're in a loading or error state ...
     if (this.state.loading) {
-      return <CoverMesage heading='Please Wait' spinner='true' />;
+      return <CoverMesage heading='Please Wait' spinner='true'/>;
     } else if (this.state.isError) {
-      return <CoverMesage heading='Problem loading application' />;
+      return <CoverMesage heading='Problem loading application'/>;
     }
 
     // AppShell component will wrap the content from the components attached
--- src/scripts/app/components/Default.js
+++ src/scripts/app/components/Default.js
@@ -1,14 +1,17 @@
 /*
-Index or default app view
+ Index or default app view
  */
 
 import React from 'react';
+import Lorem from 'react-lorem-component';
 
 export default class Index extends React.Component {
   render() {
     return (
       <div>
         <h1>Welcome to the app!</h1>
+        <Lorem count="2"/>
+        <Lorem mode="list"/>
       </div>
     );
   }
