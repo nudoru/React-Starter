951d69edf807e6671842e736a98ea2c97224aafe
Matt Perkins <mperkins@redhat.com>
2016-04-06 15:58:01 -0400

    styles

--- src/scripts/app/App.js
+++ src/scripts/app/App.js
@@ -46,6 +46,9 @@ export default class AppContainer extends React.Component {
   // Render the application view depending on loading/error or data loaded
   render() {
     // If we're in a loading or error state ...
+
+    return <AppLoadingView/>;
+
     if (this.state.loading) {
       return <AppLoadingView/>;
     } else if (this.state.isError) {
--- src/scripts/app/components/AppLoading.js
+++ src/scripts/app/components/AppLoading.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-// require('!style!css!sass!../../../sass/nudoru/components/loadingmessage.sass');
-// require('!style!css!../../../sass/nudoru/components/loadingspinner.css');
+require('!style!css!sass!../../../sass/app/components/covermessage.sass');
+require('!style!css!../../../sass/app/components/loadingspinner.css');
 
 export default class AppLoading extends React.Component {
   render() {
     return (
-      <div id="initialization__cover">
-        <div className="initialization__message">
-          <h1>Reticulating Splines</h1>
+      <div className="covermessage__cover">
+        <div className="covermessage__message">
+          <h1>Please wait ...</h1>
           <div className="loadingspinner">
             <div className="cssload-spin-box"></div>
           </div>
