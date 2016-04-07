7fc8158c2c9173d1cc6817da4e9fe5fafdae7a82
Matt Perkins <mperkins@redhat.com>
2016-04-07 10:31:25 -0400

    restructuring

--- README.md
+++ README.md
@@ -1,19 +1,15 @@
-# Webpack / React / Redux / Router - Starter
+# Express / Webpack / React / Redux / Router - Starter
  
-Just a base starting point for my React projects.
+Base starting point for my React projects.
 
 Uses WebPack and Babel 6 for bundling and building along with Hot Reloading.
+
+Structure based on http://www.christianalfoni.com/articles/2015_04_19_The-ultimate-webpack-setup
 
 WebPack is configured for:
 - SASS
 - Autoprefixing CSS back 2 versions and for IE 11
 
-- App entry point: src/scripts/app/App.js
-- Redux model: src/scripts/stores/AppStore.js
-- Router config: src/scripts/config/routes.js
 
-By default, the App will load the config.js file on start up. This external config file may be updated outside of the bundle to set configuration options for the application. 
 
-My Nudoru utility classes are included in the src/scripts/nudoru folder. Some utils developed for Nori are in the src/scripts/nori folder. 
-
-The RxJS module is included also for reactive programming.
+By default, the App will load the www/config.json file on start up. This external config file may be updated outside of the bundle to set configuration options for the application.
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,69 @@
-var path = require("path"),
+var Webpack           = require('webpack'),
+    path              = require("path"),
+    nodeModulesPath   = path.resolve(__dirname, 'node_modules'),
+    buildPath         = path.resolve(__dirname, 'www', 'build'),
+    mainPath          = path.resolve(__dirname, 'app', 'scripts', 'index.js'),
     CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  context: __dirname + "/app",
-  entry  : {
-    javascript: "./scripts/index.js",
-    html      : "./index.html"
-  },
-  output : {
-    filename: "./assets/scripts/app.js",
-    path    : "./www"
-  },
-  plugins: [
-    new CopyWebpackPlugin([
-      {from:'config.json'}
-    ])
+  // context        : __dirname + "/app",
+  // Makes sure errors in console map to the correct file
+  // and line number
+  devtool: 'eval',
+  entry  : [
+    // For hot style updates
+    'webpack/hot/dev-server',
+    // The script refreshing the browser on none hot updates
+    'webpack-dev-server/client?http://localhost:8080',
+    // Our application
+    mainPath
   ],
-  module : {
-    // TODO:
-    // Generating errors https://github.com/bestander/uglify-loader
-    // Review docs https://github.com/webpack/css-loader
-    // https://github.com/jtangelder/sass-loader
-    // Autoprefixer browser list https://github.com/ai/browserslist
+
+  // entry  : {
+  //   javascript: "./scripts/index.js",
+  //   html      : "./index.html"
+  // },
+
+  output: {
+    // We need to give Webpack a path. It does not actually need it,
+    // because files are kept in memory in webpack-dev-server, but an
+    // error will occur if nothing is specified. We use the buildPath
+    // as that points to where the files will eventually be bundled
+    // in production
+    path      : buildPath,
+    filename  : "app.js",
+    // Everything related to Webpack should go through a build path,
+    // localhost:3000/build. That makes proxying easier to handle
+    publicPath: '/build/'
+  },
+
+  module         : {
     preLoaders: [
       {
-        test: /\.js$/,
-        loader: "eslint-loader",
-        exclude: /node_modules/
+        test   : /\.js$/,
+        loader : "eslint-loader",
+        exclude: [nodeModulesPath]
       }
     ],
-    loaders: [
+    loaders   : [
       {
-        test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader : 'file-loader'
+        test  : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file-loader'
       },
       {
         test  : /\.html$/,
         loader: "file?name=[name].[ext]"
       },
       {
-        test: /\.sass$/,
+        test   : /\.sass$/,
         loaders: ["style", "css", "sass"]
       },
       {
-        test: /\.scss$/,
+        test   : /\.scss$/,
         loaders: ["style", "css", "sass"]
       },
       {
-        test: /\.css$/,
+        test  : /\.css$/,
         loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version", "ie 11"]}'
       },
       {
@@ -58,37 +74,40 @@ module.exports = {
         ]
       },
       {
-        // TODO add nudoru
-        test: /.*\/app\/.*\.js$/,
+        test  : /.*\/app\/.*\.js$/,
         loader: "uglify"
       },
       {
         test   : /\.js$/,
-        exclude: /node_modules/,
+        exclude: [nodeModulesPath],
         loaders: ["react-hot", "babel-loader"]
       }
-
     ]
   },
-  sassLoader: {
-    //https://github.com/sass/node-sass
-    indentedSyntax: true,
-    includePaths: [path.resolve(__dirname, "./app/sass/base"),
-      path.resolve(__dirname, "./app/sass/components"),
-      path.resolve(__dirname, "./app/sass/forms"),
-      path.resolve(__dirname, "./app/sass/layout"),
-      path.resolve(__dirname, "./app/sass/pages"),
-      path.resolve(__dirname, "./app/sass/themes"),
-      path.resolve(__dirname, "./app/sass/utils"),
-      path.resolve(__dirname, "./app/sass/vendor")]
+  sassLoader     : {
+    indentedSyntax: true
+    // includePaths: [path.resolve(__dirname, "./app/sass/base"),
+    //   path.resolve(__dirname, "./app/sass/components"),
+    //   path.resolve(__dirname, "./app/sass/forms"),
+    //   path.resolve(__dirname, "./app/sass/layout"),
+    //   path.resolve(__dirname, "./app/sass/pages"),
+    //   path.resolve(__dirname, "./app/sass/themes"),
+    //   path.resolve(__dirname, "./app/sass/utils"),
+    //   path.resolve(__dirname, "./app/sass/vendor")]
   },
   'uglify-loader': {
     mangle: false
   },
-  eslint: {
-    configFile: './.eslintrc',
-    quiet: false,
+  eslint         : {
+    configFile   : './.eslintrc',
+    quiet        : false,
     failOnWarning: false,
-    failOnError: false
-  }
+    failOnError  : false
+  },
+  plugins        : [
+    new Webpack.HotModuleReplacementPlugin()
+    // new CopyWebpackPlugin([
+    //   {from: 'config.json'}
+    // ])
+  ]
 };
